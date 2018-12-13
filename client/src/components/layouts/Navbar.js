d28e44c09c9c979544580e6f36d1a05af3af327b
Josema <rguezjosemanuel@gmail.com>
2018-12-13 20:08:46 +0100

    Folder image created

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PageLinks from './PageLinks'
-import LogoImage from '../../logo.png'
+import LogoImage from '../../images/logo.png'
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper #00695c teal darken-3">
             <div className="container">
-                <img style={{width: '70px', height:'60px', paddingRight:'5px'}} alt='png' src={LogoImage} />
+                <img style={{width: '70px', height:'65px', paddingRight:'5px'}} alt='png' src={LogoImage} />
                 <Link to='/' className='brand-logo'>Chuck's Jokes</Link>
                 <PageLinks />
             </div>
