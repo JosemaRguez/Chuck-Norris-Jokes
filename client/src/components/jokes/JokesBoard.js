3f66bbd43f8127eeece9da471d5da346523f6e02
Josema <rguezjosemanuel@gmail.com>
2018-12-13 01:44:34 +0100

    ScrollDown done!

--- client/src/components/jokes/JokesBoard.js
+++ client/src/components/jokes/JokesBoard.js
@@ -7,51 +7,42 @@ class JokesBoard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            numJokes: 10,
+            numJokes: 100,
             loadingState: false
         }
     }
 
     componentDidMount() {
         this.refs.iScroll.addEventListener("scroll", () => {
-            if (this.refs.iScroll.scrollTop + this.refs.iScroll.clientHeight >= this.refs.iScroll.scrollHeight) {
-                this.loadMoreJokes()
+            if (this.refs.iScroll.scrollTop + this.refs.iScroll.clientHeight <= this.refs.iScroll.scrollHeight) {
+                this.setState({ loadingState: true })
+                this.props.addJoke()
+                this.setState({ numJokes: this.state.numJokes + 10, loadingState: false })
             }
         });
     }
 
-    loadMoreJokes() {
-        console.log('dale maduro')
-        this.setState({ loadingState: true })
-        //Get more jokes
-        this.props.addJoke()
-
-        setTimeout(() => {
-            this.setState({ numJokes: this.state.numJokes + 10, loadingState: false })
-        }, 1000)
-    }
-
-
-    //this.props.addJoke
     render() {
         const { jokes } = this.props
         console.log(jokes)
+
         return (
-            <div
-                className="vc"
-                ref="iScroll"
-                style={{ height: "400px", overflow: "auto" }}
-            >
-                <div className='jokes container'>
-                    <div className='row'>
-                        <div className="col s12 m10">
-                            <JokeList jokes={jokes} />
-                            {this.state.loadingState ? <p className="loading"> loading More Items..</p> : ""}
+            <div style={{ padding: 250}}>
+                <div
+                    className="vc"
+                    ref="iScroll"
+                    style={{ height: '400px', width: '1000px', overflow: "auto" }}
+                >
+                    <div className='jokes container'>
+                        <div className='row'>
+                            <div className="col s12 m10">
+                                <JokeList jokes={jokes} />
+                                {this.state.loadingState ? <p className="loading"> loading More Items..</p> : ""}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         )
     }
 }
@@ -63,6 +54,7 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 const mapStateToProps = (state) => {
+    console.log(state.jokes.jokeList)
     return {
         jokes: state.jokes.jokeList
     }
--- client/src/components/jokes/JokesList.js
+++ client/src/components/jokes/JokesList.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const JokeList = ({ jokes }) => {
-
+    
     return (
+        
         <div className="jokes-list section">
             {jokes && jokes.map(joke => {
                 return (
-                    <div className="card z-depth-1" key={joke.id}>
-                        <div className="card-content grey-text text-darken-3 hoverable">
-                            <p>{joke.value}</p>
+                    <Link to={"/joke/" + joke.id} key={joke.id}>
+                        <div className="card z-depth-1" >
+                            <div className="card-content grey-text text-darken-3 hoverable">
+                                <p>{joke.value}</p>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 )
             })}
         </div>
