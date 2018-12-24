9f614266b0f83562fd3387c357d1a83714170239
Josema <rguezjosemanuel@gmail.com>
2018-12-24 02:08:40 +0100

    npm install

---
+++ client/build/precache-manifest.899bb8994a9e0ec29ce15089124f95df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1faa00ea934f520dc7e587492f25bd09",
+    "url": "/static/media/chuckArms.1faa00ea.png"
+  },
+  {
+    "revision": "d1a8dfeb1933349567d5da35d93d2644",
+    "url": "/static/media/balloon.d1a8dfeb.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2022c5443f46c798c337",
+    "url": "/static/js/main.2022c544.chunk.js"
+  },
+  {
+    "revision": "e078d5b579a07d42f49f",
+    "url": "/static/js/1.e078d5b5.chunk.js"
+  },
+  {
+    "revision": "2022c5443f46c798c337",
+    "url": "/static/css/main.fa1f848c.chunk.css"
+  },
+  {
+    "revision": "8576dd94e50e33309ae15dd3fe9413f8",
+    "url": "/index.html"
+  }
+];
