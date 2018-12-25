0294e51e3cda337f6b3b2285879b86815c378383
Josema <rguezjosemanuel@gmail.com>
2018-12-25 17:37:25 +0100

    Project ready to send

--- client/src/components/jokes/JokesBoard.js
+++ client/src/components/jokes/JokesBoard.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux'
 import { addJoke } from '../../store/actions/jokesActions'
 import { saveScrollPos } from '../../store/actions/scrollActions'
 import { getCategories } from '../../store/actions/categoriesActions'
-import viewportImage  from '../../images/chuckArms.png'
-import balloonImage  from '../../images/balloon.png'
+import viewportImage from '../../images/chuckArms.png'
+import balloonImage from '../../images/balloon.png'
 import '../../styles/style.css'
 
 class JokesBoard extends Component {
@@ -18,35 +18,38 @@ class JokesBoard extends Component {
         }
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.props.getCategories()
     }
 
     componentDidMount() {
         this.refs.iScroll.scrollTop = this.props.scrollLastSize
-        
+
         //Load first jokes until user can scroll on viewport
         this.refs.iScroll.addEventListener("wheel", () => {
-                if(this.refs.iScroll.scrollTop <= 0){
-                    this.setState({ loadingState: true })
+            if (this.refs.iScroll.scrollTop <= 0) {
+                this.setState({ loadingState: true }, () => {
                     this.loadJokes()
                     this.setState({ loadingState: false })
-                }
+                })
+            }
         })
 
 
         this.refs.iScroll.addEventListener("scroll", () => {
-            if (((this.refs.iScroll.scrollTop + this.refs.iScroll.clientHeight) >= (this.refs.iScroll.scrollHeight - (this.refs.iScroll.scrollHeight * 0.001))) && !this.state.loadingState ) {
+            this.props.saveScrollPos(this.refs.iScroll.scrollTop)
+            if (((this.refs.iScroll.scrollTop + this.refs.iScroll.clientHeight) >= (this.refs.iScroll.scrollHeight - (this.refs.iScroll.scrollHeight * 0.001))) && !this.state.loadingState) {
                 this.refs.iScroll.scrollTop -= (this.refs.iScroll.scrollHeight * 0.002)
-                this.setState({ loadingState: true })
-                this.loadJokes()
-                this.setState({ loadingState: false })
-                this.props.saveScrollPos(this.refs.iScroll.scrollTop)
+                this.setState({ loadingState: true }, () => {
+                    this.loadJokes()
+                    this.setState({ loadingState: false })
+                })
+                
             }
         })
     }
 
-    loadJokes = () => {
+    loadJokes() {
         this.props.addJoke()
     }
 
@@ -65,7 +68,15 @@ class JokesBoard extends Component {
 
     handleOnClick = (e) => {
         e.preventDefault()
-        this.setState({categorySelected: e.target.innerHTML})
+        this.setState({ categorySelected: e.target.innerHTML })
+    }
+
+    handleMsg = (jokes) => {
+        if (Object.keys(jokes).length === 0) {
+            return (
+                <h4 className="category-text">SCROLL HERE TO LOAD JOKES!</h4>
+            )
+        }
     }
 
     render() {
@@ -83,18 +94,18 @@ class JokesBoard extends Component {
                         ))}
                     </ul>}
                 </div>
-                
-                <img style={{width: '203px', height:'550px', left: '30px', top: '82px', position: 'absolute'}} alt='png' src={viewportImage} />
-                <img style={{width: '600px', height:'125px', left:'220px', top: '30px', position: 'relative'}} alt='png' src={balloonImage} />
+
+                <img style={{ width: '203px', height: '550px', left: '30px', top: '82px', position: 'absolute' }} alt='png' src={viewportImage} />
+                <img style={{ width: '600px', height: '125px', left: '220px', top: '30px', position: 'relative' }} alt='png' src={balloonImage} />
                 <div className="jokesContainer">
                     <div
                         className="scrollbar" id="style-1"
                         ref="iScroll"
                     >
-                        <h4 className="category-text">SCROLL HERE TO LOAD JOKES!</h4>
+                        {this.handleMsg(jokes)}
                         <div className='container'>
                             <div className="col s12 m10">
-                                <JokeList jokes={jokes} category={this.state.categorySelected}/>
+                                <JokeList jokes={jokes} category={this.state.categorySelected} />
                             </div>
                         </div>
                     </div>
